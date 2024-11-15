5f2d01f25fb1fccadb20f4a0c269673396247fa2
ChekushkinPM <x-chekushkin-pavel@yandex.ru>
2024-11-15 13:02:12 +0400

    base store

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {DecrementAction, IncrementAction, store} from "./store.ts";
+import {useEffect, useReducer} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        return store.subscribe(() => {
+            forceUpdate();
+        });
+    }, [])
+
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                counter {store.getState().counter}
+                <button onClick={() => store.dispatch({
+                    type: 'increment'
+                }satisfies IncrementAction)}>
+                    increment
+                </button>
+                <button onClick={() => store.dispatch({
+                    type: 'decrement'
+                } satisfies DecrementAction)}>
+                    decrement
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
 }
 
 export default App
